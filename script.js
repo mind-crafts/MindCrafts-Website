cb9e5b020510765e82d374d3848ee89b0551d54f
GrumpySushi <dustinkervydeguzman@gmail.com>
2025-02-24 17:17:58 +0800

    comit redo

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-    var navbar = document.querySelector("nav");
-    if (window.scrollY > 50) { // Adjust threshold
-        navbar.classList.add("shrink");
-    } else {
-        navbar.classList.remove("shrink");
-    }
-});
