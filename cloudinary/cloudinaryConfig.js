ae1391fdaf76f706c3efa0f11e3a0d602a66068e
GrumpySushi <dustinkervydeguzman@gmail.com>
2025-03-09 19:15:21 +0800

    Update cloudinaryConfig.js

--- cloudinary/cloudinaryConfig.js
+++ cloudinary/cloudinaryConfig.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "YOUR_CLOUD_NAME",
-  api_key: "YOUR_API_KEY",
-  api_secret: "YOUR_API_SECRET"
+  cloud_name: "dqxpsa3ds",
+  api_key: "431916263432213",
+  api_secret: "vKGxl6Pg34xA_CnR_zN_aNsShuk"
 });
 
 export default cloudinary;
