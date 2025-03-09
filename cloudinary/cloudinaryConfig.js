5e12caea48932d09d25ecd770f7c56cdf15230e2
GrumpySushi <dustinkervydeguzman@gmail.com>
2025-03-09 19:15:55 +0800

    Delete cloudinary/cloudinaryConfig.js

--- cloudinary/cloudinaryConfig.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-  cloud_name: "dqxpsa3ds",
-  api_key: "431916263432213",
-  api_secret: "vKGxl6Pg34xA_CnR_zN_aNsShuk"
-});
-
-export default cloudinary;
